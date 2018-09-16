3a38af2e57e8d8b0edf5c7c744994039eb9e1080
apolex212kp <43302119+apolex212kp@users.noreply.github.com>
2018-09-16 19:50:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'q'
+const prefix = 'c'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
